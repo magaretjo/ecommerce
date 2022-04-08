3156926d6129fbfc2bc269549b268ffeebb45d0d
Jo magaret <56418249+magaretjo@users.noreply.github.com>
2022-04-08 20:26:02 +0900

    FeignClient & ErrorDecoder 적용

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,15 +1,19 @@
 package com.example.userservice;
 
+import com.example.userservice.error.FeignErrorDecoder;
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
@@ -21,7 +25,14 @@ public class UserServiceApplication {
     }
 
     @Bean
+    @LoadBalanced       // RestTemplate에서 참조할 url을 Load Balancer(discovery-service)에서 찾는다.
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
 }
---
+++ user-service/src/main/java/com/example/userservice/error/FeignErrorDecoder.java
@@ -0,0 +1,37 @@
+package com.example.userservice.error;
+
+import com.fasterxml.jackson.databind.ser.BeanSerializerBuilder;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class FeignErrorDecoder implements ErrorDecoder {
+    private Environment env;
+
+    @Autowired
+    public FeignErrorDecoder(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        switch (response.status()) {
+            case 400:
+                break;
+            case 404:
+                if (methodKey.contains("getOrders")) {
+                    return new ResponseStatusException(HttpStatus.valueOf(response.status()),
+                                                        env.getProperty("order-service.exception.wrong_api"));
+                }
+                break;
+            default:
+                return new Exception(response.reason());
+        }
+        return null;
+    }
+}
